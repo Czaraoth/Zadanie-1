741633238610274ba083aa30a8eaf10fb97cd453
Cezary Chmielowiec <cchmielowiec@mp-technology.com>
2023-06-28 12:28:38 +0200

    cos tam dziala

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {PopUpService} from "./service/popup.service";
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {MarkerService} from "./service/marker.service";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MarkerService,
+  PopUpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
