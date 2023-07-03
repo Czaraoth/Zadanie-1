48dba6184286aa9f37fd007d0de61abd924c39f8
Cezary Chmielowiec <cchmielowiec@mp-technology.com>
2023-07-03 15:14:23 +0200

    1.0v

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,88 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
 import * as L from 'leaflet';
-import {MarkerService} from "./service/marker.service";
-
-const iconRetinaUrl = 'assets/marker-icon-2x.png';
-const iconUrl = 'assets/marker-icon.png';
-const shadowUrl = 'assets/marker-shadow.png';
-const iconDefault = L.icon({
-  iconRetinaUrl,
-  iconUrl,
-  shadowUrl,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  tooltipAnchor: [16, -28],
-  shadowSize: [41, 41]
-});
-L.Marker.prototype.options.icon = iconDefault;
-
+import { environment } from './environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
-export class AppComponent implements AfterViewInit{
-  private map: any;
+export class AppComponent implements AfterViewInit {
+  x:any;
+  y:any;
+  name="";
+  desc ="";
+  map: any;
+  change = false;
 
-  private initMap(): void {
-    this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
-    });
-
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-
-    tiles.addTo(this.map);
+  onName(event: any){
+    this.name=event.target.value;
+  }
+  onDesc(event: any){
+    this.desc=event.target.value;
+  }
+  changeData(){
+  this.change=true;
+  }
+  constructor() {
   }
 
-  constructor(private markerService: MarkerService) { }
-
-
-  ngAfterViewInit(): void {
-    this.initMap();
-    this.markerService.makeCapitalMarkers(this.map);
+  public ngAfterViewInit(): void {
+    this.loadMap();
   }
+
+  private getCurrentPosition(): any {
+    return new Observable((observer: Subscriber<any>) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position: any) => {
+          observer.next({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+          observer.complete();
+        });
+      } else {
+        observer.error();
+      }
+    });
+  }
+
+  private loadMap(): void {
+    this.map = L.map('map').setView([0, 0], 1);
+    this.map.doubleClickZoom.disable();
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: environment.mapbox.accessToken,
+    }).addTo(this.map);
+    const icon = L.icon({
+      iconUrl: '/assets/marker-icon.png',
+      shadowUrl: '/assets/marker-shadow.png',
+      popupAnchor: [13, 0],
+    });
+    this.getCurrentPosition()
+      .subscribe((position: any) => {
+        this.map.flyTo([position.latitude, position.longitude], 13);
+      });
+    this.map.on("dblclick", (e:any) => {
+      L.marker([e.latlng.lat, e.latlng.lng], { icon, draggable:true }).bindPopup("Name: " + this.name+","+'\n' + "Desc: " + this.desc).addTo(this.map).on('click', (e) => {
+        this.x=e.latlng.lat;
+        this.y=e.latlng.lng;
+        if(this.change){
+          e.target.setPopupContent("Name: " + this.name+","+'\n' + "Desc: " + this.desc);
+          this.change=false;
+        }
+        const popup = e.target.getPopup();
+        let content = popup.getContent();
+        content = content.split(",",2);
+        this.name=content[0].substring(6);
+        this.desc=content[1].substring(7);
+      });
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {PopUpService} from "./service/popup.service";
-import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
-import {MarkerService} from "./service/marker.service";
 
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
   ],
-  providers: [MarkerService,
-  PopUpService],
-  bootstrap: [AppComponent]
+  providers: [
+  ],
+  bootstrap: [
+    AppComponent,
+  ],
 })
-export class AppModule { }
+export class AppModule {
+}
